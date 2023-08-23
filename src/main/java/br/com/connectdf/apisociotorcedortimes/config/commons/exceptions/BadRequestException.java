e9681511593ebd01ce109873edc2eb68222dfb63
daviFNascimento <>
2023-08-23 14:54:18 -0300

    Adicionaodo not found exception

--- src/main/java/br/com/connectdf/apisociotorcedortimes/config/commons/exceptions/BadRequestException.java
+++ src/main/java/br/com/connectdf/apisociotorcedortimes/config/commons/exceptions/BadRequestException.java
@@ -4,13 +4,6 @@ import java.io.Serial;
 
 public class BadRequestException extends RuntimeException {
 
-    @Serial
-    private static final long serialVersionUID = 6769829250639411880L;
-
-    public BadRequestException() {
-        super();
-    }
-
     public BadRequestException(String message) {
         super(message);
     }
